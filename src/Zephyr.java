24d1ac424fad70d40ed447d747a4021561203565
Andrew Addis <addisandrew1@gmail.com>
2013-05-28 18:08:35 -0400

    Project setup; core libraries added.

---
+++ src/Zephyr.java
@@ -0,0 +1,20 @@
+
+import processing.core.*;
+
+public class Zephyr extends PApplet
+{	
+	public void setup()
+	{
+		size(500, 500, P2D);
+	}
+	
+	public void draw()
+	{
+		background(0xffff0000);
+	}
+	
+	public static void main(String args[])
+	{
+		PApplet.main(new String[] {"Zephyr"});
+	}
+}
