c503756f6027af97bf906d803ba784157ec61c64
Andrew Addis <addisandrew1@gmail.com>
2013-06-11 02:06:21 -0400

    Added state diagrams, organized project a bit more.

---
+++ src/Zephyr/Zephyr.java
@@ -0,0 +1,25 @@
+package Zephyr;
+
+import processing.core.*;
+
+
+@SuppressWarnings("serial")
+public class Zephyr extends PApplet
+{	
+	public static void main(String args[])
+	{
+		PApplet.main(new String[] {"Zephyr.Zephyr"});
+	}
+	
+	public void setup()
+	{
+		size(displayWidth, displayHeight, P2D);
+	}
+	
+	public void draw()
+	{
+		background(0xffff0000);
+	}
+	
+	
+}
